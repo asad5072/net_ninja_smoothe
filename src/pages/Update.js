da09789dad41f2662cc090ec76a20ffa0f73c14e
Asad <mmonir5072@gmail.com>
2024-09-18 23:42:24 +0600

    asad

--- src/pages/Update.js
+++ src/pages/Update.js
@@ -8,6 +8,30 @@ const Update = () => {
   const [title, setTitle] = useState('')
   const [method, setMethod] = useState('')
   const [rating, setRating] = useState('')
+  const [formError, setFormError] = useState('')
+
+  const handleUpdate = async (e)=>{
+    e.preventDefault();
+    if(!title || !method || !rating){
+      setFormError('Please fill up all the fields.')
+      return
+    }
+    const {data, error} = await supabase
+      .from('recipes')
+      .update({title, method, rating})
+      .eq('id', id)
+      .select()
+    
+      if(error){
+        setFormError('Please fill up all form correctly!')
+      }
+      if(data){
+        setFormError(null)
+        navigate('/')
+      }
+
+  }
+
 
   useEffect(() =>{
     const fetchSmoothies = async()=>{
@@ -32,7 +56,29 @@ const Update = () => {
   return (
     <div className="page update">
       <p>id: {id}</p>
-      
+      <form onSubmit={handleUpdate}>
+        <label htmlFor="title">Title: </label>
+        <input 
+          type="text"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label htmlFor="method">Method: </label>
+        <textarea
+          id="method"
+          value={method}
+          onChange={(e) => setMethod(e.target.value)}
+        />
+        <label htmlFor="rating">Rating:</label>
+        <input 
+          id="rating"
+          value={rating}
+          onChange={(e) => setRating(e.target.value)}
+        />
+        <button>Update</button>
+      </form>
+      {formError && <p>{formError}</p>}
     </div>
   )
 }
