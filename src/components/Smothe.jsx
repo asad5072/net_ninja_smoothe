812013741dab28f2e16d99ec7e083e66dbf6997c
Asad <mmonir5072@gmail.com>
2024-09-18 09:11:07 +0600

    asad

--- src/components/Smothe.jsx
+++ src/components/Smothe.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 const Smothe = ({smoothie})=>{
     const {title, method, rating} = smoothie
     return(
@@ -5,6 +7,11 @@ const Smothe = ({smoothie})=>{
             <p>Title: {title}</p>
             <p>Title: {method}</p>
             <p>Rating:{rating}</p>
+            <div>
+                <Link to={'/' + smoothie.id}>
+                    <i className="material-icons">edit</i>
+                </Link>
+            </div>
         </div>
     )
 }
--- src/pages/Update.js
+++ src/pages/Update.js
@@ -1,7 +1,38 @@
+import { useParams, useNavigate } from "react-router-dom"
+import {useEffect, useState} from 'react'
+import supabase from '../config/supabaseClient'
+
 const Update = () => {
+  const {id}= useParams()
+  const navigate = useNavigate()
+  const [title, setTitle] = useState('')
+  const [method, setMethod] = useState('')
+  const [rating, setRating] = useState('')
+
+  useEffect(() =>{
+    const fetchSmoothies = async()=>{
+      const {data, error} = await supabase
+        .from('recipes')
+        .select()
+        .eq('id', id)
+        .single()
+
+        if(error){
+          navigate('/', {replace: true})
+        }
+        if(data){
+          setTitle(data.title)
+          setMethod(data.method)
+          setRating(data.rating)
+          console.log(data, "data")
+        }
+    }
+    fetchSmoothies()
+  },[id, navigate])
   return (
     <div className="page update">
-      <h2>Update</h2>
+      <p>id: {id}</p>
+      
     </div>
   )
 }
